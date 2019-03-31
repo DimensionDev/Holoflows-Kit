b7761e64284ccb4d169548d11c2720e30b5de83d
Jack Works <zjwpeter@gmail.com>
2019-03-31 16:28:56 +0800

    Fix `sleep` and `timeout` for Infinity

--- src/util/sleep.ts
+++ src/util/sleep.ts
@@ -1,10 +1,9 @@
-export const sleep = (time: number) => new Promise(resolve => setTimeout(resolve, time))
-export const timeout = <T>(promise: Promise<T>, time: number) =>
-    new Promise((resolve, reject) => {
-        Promise.race<T>([
-            promise,
-            sleep(time).then<any>(() => {
-                throw new Error('Timeout')
-            }),
-        ]).then(resolve, reject)
-    })
+export const sleep = (time: number) =>
+    new Promise<void>(resolve => (Number.isFinite(time) ? setTimeout(resolve, time) : void 0))
+export const timeout = <T>(promise: Promise<T>, time: number, rejectReason?: string) =>
+    Number.isFinite(time)
+        ? Promise.race([
+              promise,
+              new Promise<T>((r, reject) => setTimeout(() => reject(new Error(rejectReason || 'timeout')), time)),
+          ])
+        : promise
