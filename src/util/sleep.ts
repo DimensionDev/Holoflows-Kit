2efeff22ee66ffc050a49e0d5216d1c2cf532ac8
Jack Works <zjwpeter@gmail.com>
2019-04-08 16:45:00 +0800

    [Breaking] Remove first generic of AsyncCall. Update document

--- src/util/sleep.ts
+++ src/util/sleep.ts
@@ -1,5 +1,16 @@
+/**
+ * Return a promise that resolved after `time` ms.
+ * If `time` is `Infinity`, it will never resolve.
+ * @param time Time to sleep. In `ms`.
+ */
 export const sleep = (time: number) =>
     new Promise<void>(resolve => (Number.isFinite(time) ? setTimeout(resolve, time) : void 0))
+/**
+ * Accept a promise and then set a timeout on it. After `time` ms, it will reject.
+ * @param promise The promise that you want to set time limit on.
+ * @param time Time before timeout. In `ms`.
+ * @param rejectReason When reject, show a reason. Defaults to `"timeout"`
+ */
 export const timeout = <T>(promise: Promise<T>, time: number, rejectReason?: string) =>
     Number.isFinite(time)
         ? Promise.race([
