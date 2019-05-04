f1616db03e646d3307a22cf3b3a4d815e99f71dd
Jack Works <zjwpeter@gmail.com>
2019-05-04 17:12:23 +0800

    Implement memorable in AutomatedTabTask

--- src/Extension/AutomatedTabTask.ts
+++ src/Extension/AutomatedTabTask.ts
@@ -2,8 +2,16 @@ import { sleep, timeout as timeoutFn } from '../util/sleep'
 import { AsyncCall } from './Async-Call'
 import { GetContext } from './Context'
 import Lock from 'concurrent-lock'
+import { memorize } from 'memorize-decorator'
 
 interface AutomatedTabTaskSharedOptions {
+    /**
+     * If the task is memorable.
+     * - true: Memorize by url and all options
+     * - false: Does not use Memory
+     * @default false
+     */
+    memorable: boolean
     /**
      * Task timeout.
      * @default 30000
@@ -12,27 +20,23 @@ interface AutomatedTabTaskSharedOptions {
 }
 export interface AutomatedTabTaskDefineTimeOptions extends AutomatedTabTaskSharedOptions {
     /**
-     * At most run `limitSize` tasks.
+     * At most run `concurrent` tasks.
      * Prevents open too many tabs at the same time.
      * @default 3
      */
-    limitSize: number
+    concurrent: number
     /**
      * A unique key
      * @default `a extension specific url.`
      */
     key: string
+    /**
+     * TTL for memorize
+     * @default 30 * 60 * 1000
+     */
+    memorizeTTL: number
 }
 export interface AutomatedTabTaskRuntimeOptions extends AutomatedTabTaskSharedOptions {
-    /**
-     * If the task is memorable.
-     * Memory will keep for 30 mins.
-     * - true: Memorize by url and all options
-     * - false: Does not use Memory
-     * - 'url': Memorize by url only
-     * @default false
-     */
-    memorable: boolean | 'url'
     /**
      * This task is important, need to start now without queue.
      */
@@ -67,11 +71,13 @@ export function AutomatedTabTask<T extends Record<string, (...args: any[]) => Pr
     taskImplements: T,
     options: Partial<AutomatedTabTaskDefineTimeOptions> = {},
 ) {
-    const { timeout: defaultTimeout, key: AsyncCallKey, limitSize } = {
+    const { timeout: defaultTimeout, key: AsyncCallKey, concurrent, memorable: defaultMemorable, memorizeTTL } = {
         ...({
             timeout: 30 * 1000,
             key: browser.runtime.getURL('@holoflows/kit:AutomatedTabTask'),
-            limitSize: 3,
+            concurrent: 3,
+            memorizeTTL: 30 * 60 * 1000,
+            memorable: false,
         } as AutomatedTabTaskDefineTimeOptions),
         ...options,
     }
@@ -110,9 +116,9 @@ export function AutomatedTabTask<T extends Record<string, (...args: any[]) => Pr
         }) as browser.runtime.onMessageVoid)
         // Register a empty AsyncCall for runtime-generated call
         const asyncCall = AsyncCall<any>({}, { key: AsyncCallKey })
-        const lock = new Lock(limitSize)
-        async function runTask(url: string, taskName: string, timeout: number, args: any[]) {
-            await lock.lock(timeout || defaultTimeout)
+        const lock = new Lock(concurrent)
+        async function runTask(url: string, taskName: string, timeout: number, withoutLock: boolean, args: any[]) {
+            if (!withoutLock) await lock.lock(timeout)
             // Create a new tab
             const tab = await browser.tabs.create({
                 active: false,
@@ -125,35 +131,34 @@ export function AutomatedTabTask<T extends Record<string, (...args: any[]) => Pr
             // Run the async call
             const task: Promise<any> = asyncCall[getTaskNameByTabId(taskName, tabId)](...args)
             try {
-                return await timeoutFn(task, timeout || defaultTimeout)
+                // ! DO NOT Remove `await`, or finally block will run before the promise resolved
+                return await timeoutFn(task, timeout)
             } finally {
-                lock.unlock()
+                if (!withoutLock) lock.unlock()
                 browser.tabs.remove(tabId)
                 delete readyMap[tabId]
             }
         }
+        const memoRunTask = memorize(runTask, { ttl: memorizeTTL })
         return (
             /** URL you want to execute the task ok */ url: string,
             options: Partial<AutomatedTabTaskRuntimeOptions> = {},
         ) => {
-            const { memorable, timeout: timeout } = {
+            const { memorable, timeout, important } = {
                 ...({
-                    memorable: false,
+                    memorable: defaultMemorable,
+                    important: false,
+                    timeout: defaultTimeout,
                 } as AutomatedTabTaskRuntimeOptions),
                 ...options,
             }
-
-            return new Proxy(
-                {},
-                {
-                    get(_, taskName) {
-                        return (...args: any[]) => {
-                            if (typeof taskName !== 'string') throw new TypeError('Key must be a string')
-                            return runTask(url, taskName, timeout, args)
-                        }
-                    },
-                },
-            ) as T
+            function runner(_: unknown, taskName: string | number | symbol) {
+                return (...args: any[]) => {
+                    if (typeof taskName !== 'string') throw new TypeError('Key must be a string')
+                    return (memorable ? memoRunTask : runTask)(url, taskName, timeout, important, args)
+                }
+            }
+            return new Proxy({}, { get: runner }) as T
         }
     } else {
         return null
