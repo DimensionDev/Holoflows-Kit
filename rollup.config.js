b24c0b89460af1540cbb093c5153ec8f27b1735d
Mingye Wang <arthur200126@gmail.com>
2019-11-11 22:37:36 +0800

    fix: apply @rollup/plugin-replace rename

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import typescript from 'rollup-plugin-typescript2'
 import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
-import replace from 'rollup-plugin-replace'
+import replace from '@rollup/plugin-replac'
 
 function parseMaybe(s) {
     return typeof s === 'string' ? JSON.parse(s) : {}
