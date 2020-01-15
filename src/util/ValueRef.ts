f2d197e3db0befc7ccd8dd60d811dfa3cde46874
Jack Works <jackworks@protonmail.com>
2020-01-15 12:24:41 +0800

    Update ValueRef.ts

--- src/util/ValueRef.ts
+++ src/util/ValueRef.ts
@@ -7,10 +7,18 @@
  * @example
  * ```ts
  * const ref = new ValueRef(64)
- * function useRef() {
- *     const [state, setState] = React.useState(ref.value)
- *     React.useEffect(() => ref.addListener(e => setState(e)))
- *     return state
+ * function useValueRef<T>(ref: ValueRef<T>) {
+ *   const { useState, useEffect } = safeReact()
+ *
+ *   const [value, setValue] = useState<T>(ref.value)
+ *   useEffect(() => {
+ *       if (ref.isEqual(value, ref.value) === false) {
+ *           // The state is outdated before the useEffect runs
+ *           setValue(ref.value)
+ *       }
+ *       return ref.addListener(v => setValue(v))
+ *   }, [ref, value])
+ *   return value
  * }
  * ref.value = 42 // useRef will receive the new value
  * ```
