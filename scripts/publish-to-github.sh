61c4e8fda146de18e902564570ca2b9d8c0a34ef
Septs <github@septs.pw>
2020-07-27 10:21:28 +0800

    update publish to github script

--- scripts/publish-to-github.sh
+++ scripts/publish-to-github.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-echo "@dimensiondev:registry=https://npm.pkg.github.com/DimensionDev" > "$HOME/.npmrc"
-
 jq '.name = "@dimensiondev/holoflows-kit"' package.json > package-modified.json
 mv package-modified.json package.json
 
 VERSION=$(jq -r '.version' package.json)
-npm --no-git-tag-version version "$VERSION-$GITHUB_RUN_NUMBER"
+npm --no-git-tag-version version "$VERSION-$BUILD_VERSION"
 npm publish
