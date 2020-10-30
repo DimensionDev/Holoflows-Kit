c4a811b1304572e7f9f10d972e00ad4e48c4e94c
Jack Works <jackworks@protonmail.com>
2020-10-30 11:35:58 +0800

    chore: upgrade dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import typescript from 'rollup-plugin-typescript2'
-import commonjs from 'rollup-plugin-commonjs'
-import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
 
 function parseMaybe(s) {
@@ -34,7 +34,6 @@ const config = {
         commonjs({
             extensions: ['.js', '.ts', '.tsx'],
             exclude: ['node_modules/lodash-es/'],
-            namedExports: {},
         }),
     ],
 }
