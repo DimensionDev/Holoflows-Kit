b816c469d0d04e2bf6eeec23c0f7784813172ee6
Jack Works <jackworks@protonmail.com>
2020-11-02 12:52:31 +0800

    tests: update test-extension

--- test-extension/setup.js
+++ test-extension/setup.js
@@ -2,4 +2,9 @@ Object.assign(globalThis, HoloflowsKit)
 try {
     chrome.devtools.panels.create('Holoflows kit test', '', 'index.html', function (panel) {})
 } catch {}
-document.write(printExtensionEnvironment() + (chrome.devtools ? '|devtoolsAPI' : ''))
+if (chrome.devtools) document.write(printExtensionEnvironment() + (chrome.devtools ? '|devtoolsAPI' : ''))
+try {
+    browser.tabs.onUpdated.addListener((tab) => {
+        browser.pageAction.show(tab)
+    })
+} catch {}
