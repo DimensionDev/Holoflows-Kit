9f1e94d3c2a353009fb41acd2ea78d6ac979eaa0
Jack Works <jackworks@protonmail.com>
2020-11-02 11:33:55 +0800

    test: update test extension

--- test-extension/setup.js
+++ test-extension/setup.js
@@ -1,1 +1,5 @@
 Object.assign(globalThis, HoloflowsKit)
+try {
+    chrome.devtools.panels.create('Holoflows kit test', '', 'index.html', function (panel) {})
+} catch {}
+document.write(printExtensionEnvironment() + (chrome.devtools ? '|devtoolsAPI' : ''))
